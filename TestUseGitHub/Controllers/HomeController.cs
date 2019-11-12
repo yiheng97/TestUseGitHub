871e702a2001dadb126ef457b7648db37fe45e06
TP041340 <TP041340@TL06-09-26>
2019-11-12 17:08:07 +0800

    test

--- TestUseGitHub/Controllers/HomeController.cs
+++ TestUseGitHub/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace TestUseGitHub.Controllers
             return View();
         }
 
+        public IActionResult displayflower()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
